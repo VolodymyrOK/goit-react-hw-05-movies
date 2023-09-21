e4c144ce13da8eaed6e8e012e9fbe4678bc58eb8
VolodymyrOK <59617490+VolodymyrOK@users.noreply.github.com>
2023-09-21 20:56:44 +0000

    Deploying to gh-pages from @ VolodymyrOK/goit-react-hw-05-movies@68043b0a7f4d53c6753fa31ebcb88ac65bfadcf8 🚀

---
+++ static/js/71.c6f4647d.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[71],{71:function(t,e,a){a.d(e,{Xmp:function(){return n}});var r=a(9983);function n(t){return(0,r.w_)({tag:"svg",attr:{viewBox:"0 0 512 512"},child:[{tag:"path",attr:{d:"M368 64L144 256l224 192V64z"}}]})(t)}}}]);
+//# sourceMappingURL=71.c6f4647d.chunk.js.map
