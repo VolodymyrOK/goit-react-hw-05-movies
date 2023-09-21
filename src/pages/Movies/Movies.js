3d31f6dce05d1bd1f7d980c5cf83e254b180d2bc
Volodymyr <vladikon70@gmail.com>
2023-09-22 00:03:20 +0200

    Update files

--- src/pages/Movies/Movies.js
+++ src/pages/Movies/Movies.js
@@ -1,34 +1,47 @@
 import { useEffect, useState } from 'react';
+import { animateScroll as scroll } from 'react-scroll';
 import { useSearchParams } from 'react-router-dom';
 import { fetchMovieQuery } from 'components/Api/Api';
 import { H1, H2 } from './Movies.styled';
 import MoviesList from 'components/MoviesList/MoviesList';
 import { MessageToast } from 'components/Messages/Messages';
 import FormApp from 'components/FormApp/FormApp';
+import { LoadMore } from 'components/LoadMore/LoadMore';
 
 const Movies = () => {
   const [searchParams] = useSearchParams();
   const queryName = searchParams.get('query') ?? '';
 
   const [results, setResults] = useState([]);
+  const [totalPages, setTotalPages] = useState(0);
+  const [page, setPage] = useState(1);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (!queryName) return;
     async function searchMovies() {
       try {
-        const dataFetch = await fetchMovieQuery(queryName);
+        setLoading(true);
+        const dataFetch = await fetchMovieQuery(queryName, page);
 
-        setResults(dataFetch.results);
+        setResults(prevResults => [...prevResults, ...dataFetch.results]);
+        setTotalPages(dataFetch.total_pages);
 
         if (dataFetch.total_pages === 0)
           return MessageToast('errorfound', 'Nothing found');
       } catch (error) {
         MessageToast('errorloading', 'OOPS! There was an error!');
       } finally {
+        setLoading(false);
       }
     }
     searchMovies();
-  }, [queryName]);
+  }, [queryName, page]);
+
+  const onloadMore = () => {
+    setPage(prev => prev + 1);
+    scroll.scrollMore(500);
+  };
 
   return (
     <>
@@ -36,6 +49,9 @@ const Movies = () => {
       <FormApp />
       {results.length > 0 && <H2>Searching results</H2>}
       {results.length > 0 && <MoviesList results={results} />}
+      {totalPages > results.length && !loading && (
+        <LoadMore onClick={onloadMore} />
+      )}
     </>
   );
 };
