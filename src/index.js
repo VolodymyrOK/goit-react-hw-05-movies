e4ed9d3820f69673c3c967421d06bc448b3f65e4
Volodymyr <vladikon70@gmail.com>
2023-10-22 15:29:03 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Scroll } from 'components/Scroll/Scroll';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/goit-react-hw-05-movies">
+    <BrowserRouter">
       <App />
       <Scroll />
     </BrowserRouter>
